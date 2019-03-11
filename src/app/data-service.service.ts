cb2ee149b6bfc86f0d4584ca92db250e0f921472
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-03-11 12:16:48 +0530

    Update data-service.service.ts

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -23,30 +23,4 @@ export class DataServiceService {
     this.table_data = data1;
     return this.table_data;
   }
-  public table_values: string[];
-  get_data() {
-    let newdata: string[];
-    newdata = this.table_data.map((ele: number, ind: number) => {
-      return Object.values(this.table_data[ind])
-    });
-    return this.table_values = newdata;
-  }
-
-  public flag: boolean = true;
-
-  sort(id: any) {
-    if (this.flag == true) {
-      console.log(this.flag);
-      this.table_data.sort(
-        function (a:any, b:any) {
-          if (a[id] > b[id] && this.flag)
-            return -1;
-          else
-            return 1;
-          return 0;
-        });
-      this.flag = false;
-    }
-  }
-
-}
+  
