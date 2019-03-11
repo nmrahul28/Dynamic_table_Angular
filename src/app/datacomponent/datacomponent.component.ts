1496be048e34248144065013f02d221bd469df82
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-03-11 15:46:36 +0530

    Update datacomponent.component.ts

--- src/app/datacomponent/datacomponent.component.ts
+++ src/app/datacomponent/datacomponent.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataServiceService } from '../data-service.service';
 import * as $ from 'jquery';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-datacomponent',
@@ -12,13 +13,19 @@ export class DatacomponentComponent implements OnInit {
   public tablehead: string[];
   public flag: boolean = true;
   public input: any;
+  public tableheading: any;
+  public userid: any;
+  public id: any;
+  public title: any;
+  public body: any;
+  public new_data: object;
   constructor() { }
   ngOnInit() {
     var extract = new DataServiceService();
     this.data = extract.dataextract();
     this.tablehead = Object.keys(this.data[0]);
-    }
-  get_data(i:number) {
+  }
+  get_data(i: number) {
     return Object.values(this.data[i]);
   }
   sorting(param) {
@@ -51,4 +58,30 @@ export class DatacomponentComponent implements OnInit {
 
     }
   }
+
+
+  ok() {
+    this.new_data = {
+      "userId": this.userid,
+      "id": this.id,
+      "title": this.title,
+      "body": this.body
+    };
+    if (this.new_data['userId']==undefined||this.new_data['id']==undefined||this.new_data['title']==undefined||this.new_data['body']==undefined) {
+      alert("Enter the data");
+    }
+    else {
+      this.data.push(this.new_data);
+
+    }
+
+  }
+  cancel(){
+    this.userid = "";
+    this.id = "";
+    this.title = "";
+    this.body = "";
+  }
+
+
 }
